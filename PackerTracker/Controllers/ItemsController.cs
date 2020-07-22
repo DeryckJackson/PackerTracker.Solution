b3fcd05bedfa9bf945f1785ac19c37dc78311cb3
Peter Grimm <pagrimm@gmail.com>
2020-07-22 11:33:36 -0700

    add folder structure, models, views, controllers, csproj files
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ PackerTracker/Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using PackerTracker.Models;
+using System.Collections.Generic;
+
+namespace PackerTracker.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/items")]
+    public ActionResult Create(string description, string price, string manufacturer)
+    {
+      Item myItem = new Item(description, int.Parse(price), manufacturer);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("items/delete")]
+    public ActionResult Delete()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }
+  }
+}
---
+++ PackerTracker/Models/Item.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+namespace PackerTracker.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Manufacturer { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+    public bool IsPacked { get; set; }
+    public int Id { get; }
+    public Item (string description, int price, string manufacturer)
+    {
+      Description = description;
+      Price = price;
+      Manufacturer = manufacturer;
+      IsPacked = false;
+      _instances.Add(this);
+      Id = _instances.Count - 1;
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId];
+    }
+
+    public static List<Item> GetUnpacked()
+    {
+      List<Item> outputList = new List<Item>();
+      foreach (Item item in _instances)
+      {
+        if (!item.IsPacked)
+        {
+          outputList.Add(item);
+        }
+      }
+      return outputList;
+    }
+  }
+}
